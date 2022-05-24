30485f362746fded1da270d6f8006f8caf0d50dd
Radu Chirilov <radulin4a@gmail.com>
2022-05-24 20:17:34 +0300

    Create Account controller

---
+++ backend/controllers/accountController.js
@@ -0,0 +1,32 @@
+const asyncHandler = require("express-async-handler");
+
+const Account = require("../models/accountModel");
+
+//  @desc   Get list of Accounts
+//  @route  GET /api/v1.0/accounts
+const getAccounts = asyncHandler(async (req, res) => {
+    const accounts = await Account.find();
+
+    res.status(200).json(accounts);
+});
+
+//  @desc   Create Account
+//  @route  POST /api/v1.0/accounts
+const createAccount = asyncHandler(async (req, res) => {
+    if (!req.body.code || !req.body.name) {
+        res.status(400);
+        throw new Error("Please provide all fields");
+    }
+
+    const account = await Account.create({
+        code: req.body.code,
+        name: req.body.name,
+    });
+
+    res.status(201).json(account);
+});
+
+module.exports = {
+    getAccounts,
+    createAccount,
+};
