e10a968c19d1f72d1444a54b8feab819aba27c9e
Radu Chirilov <radulin4a@gmail.com>
2022-05-24 21:30:07 +0300

    Create Rule model

---
+++ backend/models/ruleModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const ruleSchema = new mongoose.Schema(
+    {
+        debitCode: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: [true, "Please provide the Debit Account code"],
+            ref: 'Account'
+        },
+        creditCode: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: [true, "Please provide the Credit Account code"],
+            ref: 'Account'
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model("Rule", ruleSchema);
