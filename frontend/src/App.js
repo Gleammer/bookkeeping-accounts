5224d6c4bba00022fd1d2e591764deb89f606628
Radu Chirilov <radulin4a@gmail.com>
2022-05-26 20:56:11 +0300

    Add rule creator form and list existing Rules

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./views/Homepage";
 import Accounts from "./views/Accounts";
 import AccountPage from "./views/AccountPage";
+import Rules from "./views/Rules";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -16,7 +17,7 @@ function App() {
                 <Route path="/" element={<Homepage />} />
                 <Route path="/accounts" element={<Accounts />} />
                 <Route path="/accounts/:id" element={<AccountPage />} />
-                <Route path="/rules" element={<Homepage />} />
+                <Route path="/rules" element={<Rules />} />
             </Routes>
             <Footer />
         </Router>
--- frontend/src/views/Rules.js
+++ frontend/src/views/Rules.js
@@ -1,25 +1,76 @@
 import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRight } from "react-bootstrap-icons";
 import axios from "axios";
-//import Rule from "../components/Rule";
+import Rule from "../components/Rule";
 
 const Accounts = () => {
     const [rules, setRules] = useState([]);
+    const [accounts, setAccounts] = useState([]);
 
     useEffect(() => {
-        axios
-            .get("http://localhost:5000/api/v1.0/rules")
-            .then((res) => res.data)
-            .then((res) => setRules(res))
-            .catch((err) => console.warn(err));
+        let endpoints = [
+            "http://localhost:5000/api/v1.0/rules",
+            "http://localhost:5000/api/v1.0/accounts",
+        ];
+
+        Promise.all(endpoints.map((endpoint) => axios.get(endpoint))).then(
+            ([{ data: rules }, { data: accounts }]) => {
+                setRules(rules);
+                setAccounts(accounts);
+            }
+        );
     }, []);
+
+    const handleChange = (e) => {
+        console.log(e);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
 
     return (
         <Container>
             <Row className="my-5">
                 <Col>
                     <h1>List of Rules:</h1>
-                    
+                    {rules.length === 0 ? (
+                        <p>No rules found!</p>
+                    ) : (
+                        rules.map((rule) => (
+                            <Rule
+                                key={rule._id}
+                                id={rule._id}
+                                debitCode={rule.debitCode}
+                                creditCode={rule.creditCode}
+                                condition={rule.condition}
+                            />
+                        ))
+                    )}
+                </Col>
+                <Col>
+                    <p>Create rule:</p>
+                    <form>
+                        <select onChange={handleChange}>
+                            {accounts.map((account) => (
+                                <option value={account.code}>
+                                    {account.code}
+                                </option>
+                            ))}
+                        </select>{" "}
+                        <ArrowRight />{" "}
+                        <select onChange={handleChange}>
+                            {accounts.map((account) => (
+                                <option value={account.code}>
+                                    {account.code}
+                                </option>
+                            ))}
+                        </select>
+                        <br />
+                        <input type="text" placeholder="Condition" />
+                        <button onClick={handleSubmit}>Create Rule</button>
+                    </form>
                 </Col>
             </Row>
         </Container>
